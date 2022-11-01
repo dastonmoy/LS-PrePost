f00c6843474b5a697086e6a5b5e553500b2dff5a
Tonmoy Das <102271805+dastonmoy@users.noreply.github.com>
2022-11-01 10:42:06 -0700

    Fixed some bugs, especially renaming executable
    Several bugs were fixed in this one:
    - Directory change was not permitting the bash script to run LS-PrePost. Fixed that one by entering into the actual executable and changing the directory.
    - Running the software directly from the desktop rather than from the actual executable.

--- install_lsprepost.sh
+++ install_lsprepost.sh
@@ -1,6 +1,6 @@
 # !/bin/sh
 # Install LS-PrePost from scratch
-# Written by Tonmoy. Last update: 10/28/22
+# Written by Tonmoy. Last update: 11/01/22
 
 # Get the installer from the LSTC ftp through wget
 wget --user user --password computer https://ftp.lstc.com/user/ls-prepost/4.9/linux64/lsprepost-4.9.9-common_gtk3-14Oct2022.tgz
@@ -14,14 +14,18 @@ mv lsprepost4.9_common_gtk3/ lsprepost/
 # Now, move the folder to the home directory of user
 mv lsprepost/ ~
 
+# And change the directory name inside the actual executable of the program
+cd ~/lsprepost
+sed -i 's/lsprepost4.9_common_gtk3/lsprepost/g' lspp49
+
+
 # Also, create a shortcut of the program to the desktop for easy accessibility
 cd ~/Desktop
 echo 'cd ~/lsprepost
-./lspp49'>>lsprepost.sh
+./lspp49'>>LS-PrePost.sh
 
 # Setting the permission of the program
 chmod ugo+x lsprepost.sh
 
-# Finally, run the LS-PrePost v4.9
-cd ~/lsprepost
-./lspp49
+# Finally, running the executable from the desktop
+./LS-PrePost.sh
