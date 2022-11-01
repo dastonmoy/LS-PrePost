19fc467de46bc21476cf87c95dd451847b7aab1b
Tonmoy Das <102271805+dastonmoy@users.noreply.github.com>
2022-11-01 05:02:51 -0700

    Added comments and also renamed the directory
    - mv was added to change the name of the folder where LS-PrePost was installed
    - Comments were added for a understandable code

--- install_lsprepost.sh
+++ install_lsprepost.sh
@@ -2,8 +2,17 @@
 # Install LS-PrePost from scratch
 # Written by Tonmoy. Last update: 10/28/22
 
+# Get the installer from the LSTC ftp through wget
 wget --user user --password computer https://ftp.lstc.com/user/ls-prepost/4.9/linux64/lsprepost-4.9.9-common_gtk3-14Oct2022.tgz
+
+# Extracting the contents of the folder
 tar -xvzf ./lsprepost-4.9.9-common_gtk3-14Oct2022.tgz
-cd lsprepost4.9_common_gtk3
+
+# Change the folder name to some convenient one so that it can be accessed easily
+mv lsprepost4.9_common_gtk3/ lsprepost/
+
+# Enter inside the newly created folder
+cd lsprepost/
+
+# Finally, run the LS-PrePost v4.9
 ./lspp49
-rm -rf ~/run_ls_prepost.sh
